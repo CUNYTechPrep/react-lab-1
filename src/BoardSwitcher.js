ab38c4bab80fe5d446963f1dec4eb74b4ba05f1f
Rachel Minkowitz <rhminkowitz@gmail.com>
2021-09-15 14:06:35 -0400

    Toggle working

--- src/BoardSwitcher.js
+++ src/BoardSwitcher.js
@@ -1,3 +1,6 @@
+//This program allows us to cycle through frames by allowing us to
+//Toggle our frame on the next board when the toggle button is clicked
+
 import React from 'react';
 
 class Board extends React.Component {
@@ -15,25 +18,35 @@ class Board extends React.Component {
 }
 
 class BoardSwitcher extends React.Component {
-  
   constructor(props){
     super(props);
-    this.state={
+    this.state = {
       selected : 0 
     };
   }
 
+  //response of the click function
+
   respondToMe(event){
-    this.setState({
-      selected: function(){
-        if(this.state.selected + 1 <= this.numBoards){
-        return this.state.index + 1;
-      
-      }else{
-        return 0;
+
+    //I call a function inside setState which is passed the 
+    //component state from when that setState was called - which I call previousState
+
+    this.setState((previousState) => {
+
+      //if the selected previousState (0-2) is equal to this.props.numBoards-1 
+      //(because selected begins from 0, otherwise we would have to do an extra click)
+      //we restart the selected state at 0, otherwise we increment the state
+
+      if (previousState.selected === this.props.numBoards-1){
+        return{
+          selected : 0
+        }
       }
-    }
-    });
+      return {
+        selected: previousState.selected +1
+      }
+    }); 
   }
 
   render() {
@@ -41,8 +54,9 @@ class BoardSwitcher extends React.Component {
     for (let ii = 0; ii < this.props.numBoards; ii++) {
       let isSelected = ii === this.state.selected;
       boards.push(
-        <Board index={ii} selected={isSelected} key={ii} /> //isSelected must increment
+        <Board index={ii} selected={isSelected} key={ii} /> 
       );
+      console.log(this.state.selected)
     }
 
     return (
