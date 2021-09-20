32baf8d4a7aef6f7a0f0e9446e4c418890e1c3b5
JessicaPlotsker <jessep0298@gmail.com>
2021-09-19 20:46:26 -0400

    finished lab

--- src/BoardSwitcher.js
+++ src/BoardSwitcher.js
@@ -15,10 +15,21 @@ class Board extends React.Component {
 }
 
 class BoardSwitcher extends React.Component {
+  constructor(props) {
+    super(props); // required so that props work
+    this.state = {
+      clicks: 0,
+    };
+  }
+  handleClick(event) {
+    this.setState({
+      clicks: (this.state.clicks + 1)%3
+    });
+  }
   render() {
     let boards = [];
     for (let ii = 0; ii < this.props.numBoards; ii++) {
-      let isSelected = ii === 0;
+      let isSelected = ii === this.state.clicks;
       boards.push(
         <Board index={ii} selected={isSelected} key={ii} />
       );
@@ -27,7 +38,7 @@ class BoardSwitcher extends React.Component {
     return (
       <div>
         <div className="boards">{boards}</div>
-        <button>Toggle</button>
+        <button onClick={(e) => this.handleClick(e)}>Toggle</button>
       </div>
     );
   }
