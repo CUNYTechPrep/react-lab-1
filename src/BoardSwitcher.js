9aa5849730f98d1c7773051ea3416bdc1a460f87
Melissa Persaud <melissapersaud17.mp@gmail.com>
2021-09-20 14:57:27 -0400

    explained what each section does in the Boardswitcher and figured out how to add event handling to the button

--- src/BoardSwitcher.js
+++ src/BoardSwitcher.js
@@ -1,5 +1,8 @@
 import React from 'react';
 
+//Note: using the React.Component we can define a component and we must use the render() method
+//other methods are optional
+
 class Board extends React.Component {
   render() {
     let className = "board";
@@ -15,19 +18,24 @@ class Board extends React.Component {
 }
 
 class BoardSwitcher extends React.Component {
+  toggleClick(){
+     console.log('melissa');
+   }
+
   render() {
     let boards = [];
-    for (let ii = 0; ii < this.props.numBoards; ii++) {
-      let isSelected = ii === 0;
+    for (let ii = 0; ii < this.props.numBoards; ii++) { //numBoards is passed in through the index.js file and sets this to 3 
+      let isSelected = ii === 0; // this controls which board is selected this also determines the value isSelected (true or false)
       boards.push(
-        <Board index={ii} selected={isSelected} key={ii} />
+        <Board index={ii} selected={isSelected} key={ii} /> //index displays the number on the board; selected highlights the board; 
       );
     }
 
-    return (
+
+    return ( //this displays the boards and the toggle button
       <div>
         <div className="boards">{boards}</div>
-        <button>Toggle</button>
+        <button onClick={this.toggleClick}>Toggle</button> 
       </div>
     );
   }
