ec2c037487122e6a7558c48b427aaa88b6aaafe5
btpf <bretpapkoff@Gmail.com>
2021-09-21 19:34:15 -0400

    Complete Lab

--- src/BoardSwitcher.js
+++ src/BoardSwitcher.js
@@ -15,10 +15,20 @@ class Board extends React.Component {
 }
 
 class BoardSwitcher extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {counter: 0, binaryMode: false};
+  }
   render() {
     let boards = [];
     for (let ii = 0; ii < this.props.numBoards; ii++) {
-      let isSelected = ii === 0;
+      let isSelected;
+      if(this.state.binaryMode){
+        isSelected = (((this.state.counter % 2**(this.props.numBoards))) >> (ii)) & 1;
+      }else{
+        isSelected = this.state.counter % this.props.numBoards === ii;
+      }
+      
       boards.push(
         <Board index={ii} selected={isSelected} key={ii} />
       );
@@ -27,7 +37,8 @@ class BoardSwitcher extends React.Component {
     return (
       <div>
         <div className="boards">{boards}</div>
-        <button>Toggle</button>
+        <button onClick={()=> this.setState({counter: this.state.counter + 1})}>Toggle</button>
+        <button onClick={()=> this.setState({binaryMode: !this.state.binaryMode})}>ToggleBinaryMode</button>
       </div>
     );
   }
