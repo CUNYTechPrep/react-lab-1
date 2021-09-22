13bf6ccd7b287a535e86263c5f36311974fce9c2
Parminderk766 <51168168+Parminderk766@users.noreply.github.com>
2021-09-22 15:53:15 -0400

    ParminderReactLab

--- src/BoardSwitcher.js
+++ src/BoardSwitcher.js
@@ -15,19 +15,39 @@ class Board extends React.Component {
 }
 
 class BoardSwitcher extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      selected: 0
+    }
+  } 
+
+  toggle=()=>{
+    if(this.state.selected !== 2)
+      this.setState({
+        selected: this.state.selected+1
+      })
+    if(this.state.selected === 2){
+      this.setState({
+        selected: 0
+      })
+    }
+  }
+
   render() {
     let boards = [];
+    
     for (let ii = 0; ii < this.props.numBoards; ii++) {
-      let isSelected = ii === 0;
+      let isSelected = ii === this.state.selected;
       boards.push(
         <Board index={ii} selected={isSelected} key={ii} />
       );
     }
-
+    
     return (
       <div>
         <div className="boards">{boards}</div>
-        <button>Toggle</button>
+        <button onClick={this.toggle}>Toggle</button>
       </div>
     );
   }
