b45bc72f33fffc16f03ab9a9d0f5f3944b1b545f
Kev <haokliao@gmail.com>
2021-09-22 19:06:26 -0400

    completed in class

--- src/BoardSwitcher.js
+++ src/BoardSwitcher.js
@@ -2,42 +2,48 @@ import React from 'react';
 
 class Board extends React.Component {
     render() {
-      let className = "board";
-      if (this.props.selected) {
-        className += " selected";
-      }
-      return (
-        <div className={className}>
-          {this.props.index + 1}
-        </div>
-      );
-    }
+        let className = "board";
+        if (this.props.selected) {
+            className += " selected";
+        }
+        return (
+            <div className={className}>
+        {this.props.index + 1}
+            </div>
+        );
+        }
 }
 
 class BoardSwitcher extends React.Component{
-    currentBoard= {start:0}
 
-    //pretty sure the issue is here 
-    onToggleClick=(e)=>{
-        this.setState({
-        start: (this.currentBoard.start+1) % 3 
-        })
+    constructor(props){
+        super(props)
+
+        this.state={
+            selected:0
+        }
     }
 
-  render() {
+    handleButtonClick(){
+        this.setState({selected: this.state.selected + 1} 
+        )}
+
+render() {
     let boards = [];
 
-        for (let ii = 0; ii < this.props.numBoards; ii++) {
-        let isSelected = ii === this.currentBoard.start;
+    for (let ii = 0; ii < this.props.numBoards; ii++) {
+        //issSelected becomes selected board which starts from 0 % the number of boards 
+
+        let isSelected =  this.state.selected % this.props.numBoards === ii;
         boards.push(
             <Board index={ii} selected={isSelected} key={ii} />
-            );
-        }
+        );
+    }
 
         return (
         <div>
             <div className="boards">{boards}</div>
-            <button onClick ={this.onToggleClick}>Toggle</button>
+            <button onClick ={()=>this.handleButtonClick()}>Toggle</button>
         </div>
         );
         
