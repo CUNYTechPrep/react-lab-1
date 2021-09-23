5f46033c1f5a03539ddb0c4c03c1491dcdee06cb
auto-kad <abedin.kadir@gmail.com>
2021-09-23 02:47:42 -0400

    finished lab

--- src/BoardSwitcher.js
+++ src/BoardSwitcher.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ToggleButton from './ToggleButton'
 
 class Board extends React.Component {
   render() {
@@ -15,22 +16,59 @@ class Board extends React.Component {
 }
 
 class BoardSwitcher extends React.Component {
+
+  // constructor(props){
+  //   super(props);
+  //   this.state = {boardToToggle: 0}
+  // }
+  
+  state = {
+    boardToToggle: 0
+  }
+
+  toggleButton = () => {
+    this.setState({
+      boardToToggle: (this.state.boardToToggle + 1) % 3
+    });
+  }
+
+  
+
+
+
   render() {
     let boards = [];
+    console.log("value of toggle:", this.state.boardToToggle);
+
+    // 0, 1, 2; will only be true/1 when ii is 0 via the loop -> need to cycle through === 0, 1, 2 somehow in the isSelected statement
+    // store some state, when you toggle the button, add + 1, then do mod 3, so that the numbers cycle from 0->2
+
     for (let ii = 0; ii < this.props.numBoards; ii++) {
-      let isSelected = ii === 0;
+      let isSelected = ii === this.state.boardToToggle;
       boards.push(
         <Board index={ii} selected={isSelected} key={ii} />
       );
     }
 
     return (
-      <div>
+
+      // <ToggleButton>
+      //   {/* {boards} */}
+        
+      // </ToggleButton>
+
+        <div>
         <div className="boards">{boards}</div>
-        <button>Toggle</button>
+        <button onClick={this.toggleButton}>Toggle</button>
       </div>
+      
+    
+      
+
     );
+
   }
 }
 
 export default BoardSwitcher;
+
---
+++ src/ToggleButton.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export default class ToggleButton extends React.Component {
+    render() {
+        return <div className="boards"> testing </div>
+    }
+}
+
