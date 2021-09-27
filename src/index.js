d88ebe98d890de44cf415a84158babbcb03469f7
LuisBcode <ab.luis04@gmail.com>
2021-09-26 21:55:09 -0400

    Lab completed

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import BoardSwitcher from './BoardSwitcher';
 import './index.css';
 
+
+
 ReactDOM.render(
-  <BoardSwitcher numBoards={3} />,
+  <BoardSwitcher numBoards={5} />,
   document.getElementById('root')
 );
