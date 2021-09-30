7403c4e58bdd4670fb8d3eb2bf197431668f0926
ibnat136 <ibnatnuha42@gmail.com>
2021-09-29 21:50:12 -0400

    Updated board switcher TA:Silu Instructor: Edgardo

--- src/BoardSwitcher.js
+++ src/BoardSwitcher.js
@@ -1,23 +1,35 @@
 import React from 'react';
+//import ReactDOM from 'react-dom'
+
 
 class Board extends React.Component {
-  render() {
-    let className = "board";
-    if (this.props.selected) {
-      className += " selected";
-    }
-    return (
-      <div className={className}>
-        {this.props.index + 1}
-      </div>
-    );
+  // constructor(props) 
+  //   super(props);
+  state={
+    start_at:0
   }
+
+  handleClick=(event)=>{
+    this.setState({start:(this.state.start_at+1) % 3})
+  }
+  // render() {
+  //   let className = "board";
+  //   if (this.props.selected) {
+  //     className += " selected";
+  //   }
+  //   return (
+  //     <div className={className}>
+  //       {this.props.index + 1}
+  //     </div>
+  //   );
+  // }
 }
 
 class BoardSwitcher extends React.Component {
   render() {
     let boards = [];
     for (let ii = 0; ii < this.props.numBoards; ii++) {
+      
       let isSelected = ii === 0;
       boards.push(
         <Board index={ii} selected={isSelected} key={ii} />
